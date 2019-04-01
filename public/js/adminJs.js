28f82fe58197e7fc6dc5765845fba4bb0eb12571
anacarde <lazare.monin@gmail.com>
2019-04-01 15:37:49 +0200

    ajout des boutons manuels de retrait des divs admin

--- public/js/adminJs.js
+++ public/js/adminJs.js
@@ -6,14 +6,37 @@ function AdminEvents() {
     this.addChapterButton = document.getElementById('add_chapter_button');
     this.chaptersButton = document.getElementById('chapters_list_button');
     this.commentsButton = document.getElementById('comments_button');
+    this.closeCross = document.getElementsByClassName('close_cross');
 
     this.addChapterDiv = document.getElementById('add_chapter_form');
-    this.chaptersTable = document.getElementById('chapters_table');
-    this.commentsTable = document.getElementById('comments_table');
+    this.chaptersTable = document.getElementById('chapters_list_div');
+    this.commentsTable = document.getElementById('comments_list_div');
 
     this.addChapButtChecked = false; 
     this.chapButtChecked = false;
     this.comButtChecked = false;
+
+    this.closeAdminDivFn = function() {
+
+        if(self.addChapButtChecked == true){
+                self.addChapterButton.classList.remove("set_css");
+                self.addChapterDiv.classList.add("hide");
+                self.addChapButtChecked = false; 
+                return;
+        }
+        if(self.chapButtChecked == true){
+            self.chaptersButton.classList.remove("set_css");
+            self.chaptersTable.classList.add("hide");
+            self.chapButtChecked = false;
+            return;
+        }
+        if(self.comButtChecked == true){
+            self.commentsButton.classList.remove("set_css");
+            self.commentsTable.classList.add("hide");
+            self.comButtChecked = false;
+            return;
+        }
+    }
 
     this.displayEvents = function() {
 
@@ -54,26 +77,13 @@ function AdminEvents() {
             self.comButtChecked = true;
         });
 
+        for (var i = 0 ; i < this.closeCross.length ; i++) {
+            this.closeCross[i].addEventListener("click", this.closeAdminDivFn);
+        }
+
         window.addEventListener('keydown', function(e){
             if(e.keyCode == 27) {
-                if(self.addChapButtChecked == true){
-                    self.addChapterButton.classList.remove("set_css");
-                    self.addChapterDiv.classList.add("hide");
-                    self.addChapButtChecked = false; 
-                    return;
-                }
-                if(self.chapButtChecked == true){
-                    self.chaptersButton.classList.remove("set_css");
-                    self.chaptersTable.classList.add("hide");
-                    self.chapButtChecked = false;
-                    return;
-                }
-                if(self.comButtChecked == true){
-                    self.commentsButton.classList.remove("set_css");
-                    self.commentsTable.classList.add("hide");
-                    self.comButtChecked = false;
-                    return;
-                }
+                self.closeAdminDivFn();
             }
         });
     }
--- src/view/adminView.php
+++ src/view/adminView.php
@@ -22,7 +22,8 @@
         <button id="chapters_list_button" class="button_admin"> Gérer mes chapitres </button>
         <button id="comments_button" class="button_admin"> Gérer mes commentaires </button>
     </div>
-    <form id="add_chapter_form" class="table_admin hide"  action="/add" method="post">
+    <form id="add_chapter_form" class="admin_div hide"  action="/add" method="post">
+        <button class="close_cross"> X </button>
         <div id="chap_nb_div">
             <label for="nb_input" class="form_label"> Numéro du chapitre : 
                 <input type="text" id="nb_input" name="ChapterNumber" />
@@ -35,31 +36,35 @@
         <textarea id="chapter_editor" name="Content"></textarea>
         <input type="submit" id="send_btn" value="Envoi de mon chapitre" />
     </form>
-    <table id="chapters_table" class="table_admin hide">
-        <tr>
-            <th> Chapitre n° </th>
-            <th> Titre </th>
-            <th> Date d'ajout </th>
-            <th> Dernière modification </th>
-            <th colspan="2"> Action </th>
-        </tr>
-        <?php
-        foreach($this->chaptersList as $key => $chapter)
-        {
-        ?>
-        <tr>
-            <td> <?= $chapter->getChapterNumber() ?> </td>
-            <td> <?= $chapter->getTitle() ?> </td>
-            <td> <?= $chapter->getDateAdd() ?> </td>
-            <td> <?= $chapter->getDateModif() ?> </td>
-            <td class="action modif"> <button> Modifier </button> </td>
-            <td class="action supp"> <button> Supprimer </button> </td>
-        </tr>
-        <?php
-        }
-        ?>
-    </table>
-<!--     <form id="modif_chapter_form" class="hide" action="/update" method="post">
+    <div id="chapters_list_div" class="admin_div hide">
+        <button class="close_cross"> X </button>
+        <br/>
+        <table id="chapters_table" class="table_admin">
+            <tr>
+                <th> Chapitre n° </th>
+                <th> Titre </th>
+                <th> Date d'ajout </th>
+                <th> Dernière modification </th>
+                <th colspan="2"> Action </th>
+            </tr>
+            <?php
+            foreach($this->chaptersList as $key => $chapter)                
+            {
+            ?>
+            <tr>
+                <td> <?= $chapter->getChapterNumber() ?> </td>
+                <td> <?= $chapter->getTitle() ?> </td>
+                <td> <?= $chapter->getDateAdd() ?> </td>
+                <td> <?= $chapter->getDateModif() ?> </td>
+                <td class="action modif"> <button> Modifier </button> </td>
+                <td class="action supp"> <button> Supprimer </button> </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
+<!--<form id="modif_chapter_form" class="hide" action="/update" method="post">
         <div id="chap_nb_div">
             <label for="nb_input" class="form_label"> Numéro du chapitre : 
                 <input type="text" id="nb_input" name="ChapterNumber" />
@@ -72,29 +77,33 @@
         <textarea id="chapter_editor" name="Content"></textarea>
         <input type="submit" id="send_btn" value="Envoi de mon chapitre" />
     </form> -->
-    <table id="comments_table" class="table_admin hide">
-        <tr>
-            <th> Commentaires au chapitre n° </th>
-            <th> Date </th>
-            <th> Auteur </th>
-            <th> Contenu </th>
-            <th colspan="2"> Action </th>
-        </tr>
-        <?php
-        foreach($this->commentsList as $key => $comment)
-        {
-        ?>
-        <tr>
-            <td> <?= $comment->getCommentChapter(); ?> </td>
-            <td> <?= $comment->getDateAdd(); ?>  </td>
-            <td> <?= $comment->getAuthor(); ?>  </td>
-            <td> <?= $comment->getContent(); ?>  </td>
-            <td class="action modif"> <button> Modifier </button></td>
-            <td class="action supp"> <button> Supprimer </button> </td>
-        </tr>
-        <?php
-        }
-        ?>
-    </table>
+    <div id="comments_list_div" class="admin_div hide">
+        <button class="close_cross"> X </button>
+        <br/>
+        <table id="comments_table" class="table_admin">
+            <tr>
+                <th> Commentaires au chapitre n° </th>
+                <th> Date </th>
+                <th> Auteur </th>
+                <th> Contenu </th>
+                <th colspan="2"> Action </th>
+            </tr>
+            <?php
+            foreach($this->commentsList as $key => $comment)
+            {
+            ?>
+            <tr>
+                <td> <?= $comment->getCommentChapter(); ?> </td>
+                <td> <?= $comment->getDateAdd(); ?>  </td>
+                <td> <?= $comment->getAuthor(); ?>  </td>
+                <td> <?= $comment->getContent(); ?>  </td>
+                <td class="action modif"> <button> Modifier </button></td>
+                <td class="action supp"> <button> Supprimer </button> </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
     <script src="/js/adminJs.js"></script>
 </body>
