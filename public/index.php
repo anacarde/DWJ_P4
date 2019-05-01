7ab9639cb16ebe2e1f217e1a00b8033b3ca37032
anacarde <lazare.monin@gmail.com>
2019-05-01 15:50:20 +0200

    début de tentative de mise sur le web, erreurs imprévues, préparation de corrections.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
+echo "indexA";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__."/../vendor/autoload.php";
+
+echo "indexB";
 
 use App\Router\Router;
 use Src\Model\Manager;
@@ -8,18 +14,29 @@ use Src\Model\ChaptersManager;
 use Src\Controller\Controller;
 use Zend\Diactoros\ServerRequestFactory;
 
+echo "indexC";
+
 $request = ServerRequestFactory::fromGlobals();
+
+echo "indexD";
 
 $router = new Router($request);
 
+echo "indexE";
+
 $router->loadYaml(__DIR__."/../config/routing.yml");
+
+echo "indexF";
 
 try {
     $route = $router->getRouteByRequest();
 
     $route->call($request);
 
+    echo "indexG";
+
 } catch (\Exception $e) {
+    echo "indexH";
 }
 
 
